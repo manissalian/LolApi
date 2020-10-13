6d0385a9afb920a66064e7292a32d6d65d91948a
Raffi <rmanisalian@cinemoz.com>
2020-10-14 00:30:22 +0200

    first commit

---
+++ LoLApi/LoLApi.h
@@ -0,0 +1,19 @@
+//
+//  LoLApi.h
+//  LoLApi
+//
+//  Created by Apple on 10/14/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoLApi.
+FOUNDATION_EXPORT double LoLApiVersionNumber;
+
+//! Project version string for LoLApi.
+FOUNDATION_EXPORT const unsigned char LoLApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoLApi/PublicHeader.h>
+
+
