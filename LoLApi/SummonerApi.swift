db8cad44ed55161c4893aab98f88d47fc425c3b8
Raffi <rmanisalian@cinemoz.com>
2020-10-16 21:23:11 +0200

    add getSummonerByAccountId API

--- LoLApi/SummonerApi.swift
+++ LoLApi/SummonerApi.swift
@@ -21,6 +21,31 @@ public struct SummonerApi {
         let path = "/by-name/\(name)"
         let urlString = "\(lolApi.host)/\(endPoint)\(path)"
 
+        self.getSummoner(urlString: urlString) { summoner in
+            completion(summoner)
+        }
+    }
+
+    public func getSummonerByAccountId(accountId: String, completion: @escaping (_ summoner: Summoner?) -> Void) {
+        guard let lolApi = self.lolApi else {
+            completion(nil)
+            return
+        }
+
+        let path = "/by-account/\(accountId)"
+        let urlString = "\(lolApi.host)/\(endPoint)\(path)"
+
+        self.getSummoner(urlString: urlString) { summoner in
+            completion(summoner)
+        }
+    }
+
+    func getSummoner(urlString: String, completion: @escaping (_ summoner: Summoner?) -> Void) {
+        guard let lolApi = self.lolApi else {
+            completion(nil)
+            return
+        }
+
         Request.httpGet(key: lolApi.apiKey, urlString: urlString) { data in
             guard let data = data else {
                 completion(nil)
--- LoLApiTests/UnitTests/SummonerApiTests.swift
+++ LoLApiTests/UnitTests/SummonerApiTests.swift
@@ -10,11 +10,29 @@ import XCTest
 import LoLApi
 
 class SummonerApiTests: XCTestCase {
+    let lolApi = LolApi(key: TESTING_API_KEY, server: LolServer.EUW)
+
     func testGetSummonerByName() throws {
         let expect = expectation(description: "Got summoner successfully")
-        let lolApi = LolApi(key: TESTING_API_KEY, server: LolServer.EUW)
 
         lolApi.summoner.getSummonerByName(name: "callbackfunction") { summoner in
+            if summoner == nil {
+                XCTFail("Error getting summoner")
+            }
+
+            expect.fulfill()
+        }
+
+        let result = XCTWaiter.wait(for: [expect], timeout: 10)
+        if result == XCTWaiter.Result.timedOut {
+            XCTFail("Timed out")
+        }
+    }
+
+    func testGetSummonerByAccountId() throws {
+        let expect = expectation(description: "Got summoner successfully")
+
+        lolApi.summoner.getSummonerByAccountId(accountId: "4CTJNXQHPPoZx49ph3aeffovrf9d3KQp_-2G8pDDZI3iLFs") { summoner in
             if summoner == nil {
                 XCTFail("Error getting summoner")
             }
